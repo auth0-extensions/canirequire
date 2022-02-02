fde127307a871e25059cd66b81bfec295507e789
Chris Geihsler <chris.geihsler@auth0.com>
2022-02-02 12:14:56 -0500

    feat: add support for node 16

--- src/fetch_modules.js
+++ src/fetch_modules.js
@@ -1,16 +1,17 @@
-export default function(nodeVersion) {
+export default function (nodeVersion) {
   if (window.wtModuleList) {
-    return $.Deferred().resolve(window.wtModuleList)
+    return $.Deferred().resolve(window.wtModuleList);
   }
 
-  const defaultNodeVersion = 12;
+  const defaultNodeVersion = 16;
   const nodeUrls = {
-    "8": "https://auth0-internal.us8.webtask.io/canirequire",
-    "12": "https://auth0-internal.us12.webtask.io/canirequire",
+    8: "https://auth0-internal.us8.webtask.io/canirequire",
+    12: "https://auth0-internal.us12.webtask.io/canirequire",
+    16: "https://auth0-internal.us12.webtask.io/canirequire",
   };
 
   return $.ajax({
-      url: nodeUrls[nodeVersion] || nodeUrls[defaultNodeVersion],
-      method: 'GET'
-    });
+    url: nodeUrls[nodeVersion] || nodeUrls[defaultNodeVersion],
+    method: "GET",
+  });
 }
