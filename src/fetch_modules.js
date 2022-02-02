10439e23d4ddd9be6cb279f4436334c004fdeb1e
Chris Geihsler <chris.geihsler@auth0.com>
2022-02-02 12:26:45 -0500

    chore: use the runtime header instead of URLs

--- src/fetch_modules.js
+++ src/fetch_modules.js
@@ -4,14 +4,13 @@ export default function (nodeVersion) {
   }
 
   const defaultNodeVersion = 16;
-  const nodeUrls = {
-    8: "https://auth0-internal.us8.webtask.io/canirequire",
-    12: "https://auth0-internal.us12.webtask.io/canirequire",
-    16: "https://auth0-internal.us12.webtask.io/canirequire",
-  };
+  const url = "https://auth0-internal.us.webtask.io/canirequire";
 
   return $.ajax({
-    url: nodeUrls[nodeVersion] || nodeUrls[defaultNodeVersion],
+    url,
     method: "GET",
+    headers: {
+      "x-wt-runtime": `node${nodeVersion}`,
+    },
   });
 }
